4c38375293954890d74be8b6d4409e31033fe669
Alexd76340 <dufossealexis@gmail.com>
2025-08-03 21:59:06 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -5,14 +5,46 @@ const softQuestions = [
   "Quel est ton animal préféré ?",
   "Quelle est ta saison préférée ?",
   "Quel est ton film favori ?",
-  "Quel pays rêves-tu de visiter ?"
+  "Quel pays rêves-tu de visiter ?",
+  "Quel est ton acteur préféré ?",
+  "Quelle est ton actrice préférée ?",
+  "Quelle est ta série préférée ?",
+  "Quelle est ta plus grande qualitée ?",
+  "Quel est ton plus gros défaut ?",
+  "Quel est ton deuxième prénom ?",
+  "Quel surnom ton partenaire te donne t'il ?",
+  "Quelle partie de ton corp tu aime le plus ?",
+  "Quelle partie du corp tu aime le moin ?",
+  "Quelle est ton plus grand rêve ?",
+  "Ton jeu vidéo préférée ?",
+  "Ton sport préféré ?",
+  "Ton souvenir préférée avec ton copain / copine ?",
+  "Le plus beau cadeau qu'il ou qu'elle ta fait ?",
+  "Comment imagine tu ta vie dans 5 ans ?",
+  "Quel est le moment de ta relation que tu aimerais revivre ?",
+  "Ta chansson préférée ?",
+  "La première chose que tu regardé chez lui / elle ?"
 ];
 
 const hotQuestions = [
   "Quel est ton plus grand fantasme ?",
-  "As-tu déjà eu une expérience à trois ?",
   "Quelle est ta zone érogène préférée ?",
   "Quelle est ta position favorite ?",
-  "As-tu déjà rêvé de moi de façon coquine ?",
-  "Quel est ton endroit préféré pour faire l'amour ?"
+  "Quel est ton endroit préféré pour faire l'amour ?",
+  "Avec quel acteur/actrice tu voudrait coucher ?",
+  "L'endroit le plus insolite ou tu a fait l'amour ?",
+  "Ta plus grosse honte au lit ?",
+  "La chose la plus hard que tu as faite ?",
+  "L'anectode la plus drole en fesant l'amour ?",
+  "La partie du corp que tu préfére chez l'autre ?"
+  "Ta position sexuelle préférée ?",
+  "L'endroit le plus insolite ou tu as fait l'amour ?",
+  "Si ton partenaire te chuchote à l'oreille maintenant, tu voudrais entendre quoi ?",
+  "Qu'est ce que tu aimerais que ton partenaire fasse le plus au lit ?",
+  "Comment tu cherche ton partenaire quand tu as envie ?",
+  "Quelle a été la première pensée coquine quand tu as vu ton partenaire ?",
+  
+  
+  
+  
 ];
